a4ca498c4c2fe9c6b8a52b40968931aa108b5268
Blake Scala <blakescala@gmail.com>
2016-12-19 11:21:02 -0800

    add unfinished login method and create account intent

---
+++ app/src/main/java/com/epicodus/blake/bombdefuser/ui/CreateAccountActivity.java
@@ -0,0 +1,18 @@
+package com.epicodus.blake.bombdefuser.ui;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.blake.bombdefuser.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+    }
+}
